93edd7970371cd52d736bca7409ec9553c3ba578
Sandro Torres <sandrotorres.ti@gmail.com>
2018-09-24 07:47:29 -0300

    Add outros tipos de input

--- exercicios-web/html/exercicios/form/server.js
+++ exercicios-web/html/exercicios/form/server.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parser') // Importa o body-parser
 
 app.use(bodyParser.urlencoded({ extended: true})) // associa app.use com o body-parser no modo extendido
 
+// Atende uma requisição do tipo post em cima de usuarios
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
     resp.send('<h1>Parabéns</h1>')
 })
 
-app.listen(3003)
+app.listen(3003) // Informa a porta que vai ser usada
