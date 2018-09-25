cdb9c4e8fdf5d5c446130ad663306229be9ec354
Sandro Torres <sandrotorres.ti@gmail.com>
2018-09-25 06:20:15 -0300

    Finalizando Formilarios

--- exercicios-web/html/exercicios/form/server.js
+++ exercicios-web/html/exercicios/form/server.js
@@ -7,7 +7,14 @@ app.use(bodyParser.urlencoded({ extended: true})) // associa app.use com o body-
 // Atende uma requisição do tipo post em cima de usuarios
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns, registro incluido com sucesso!!!</h1>')
+})
+
+// Atende uma requisição do tipo post em cima de usuarios
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns, alteração realizada com sucesso!!!</h1>')
 })
 
 app.listen(3003) // Informa a porta que vai ser usada
